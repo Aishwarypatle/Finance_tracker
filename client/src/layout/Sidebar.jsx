708362600b1d8aae1d907008ab5beb8366da0361
Aishwary <aishwarypatle@26gmail.com>
2024-09-11 17:55:12 +0530

    layout updated

---
+++ client/src/layout/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 h-screen bg-gray-800 text-white flex flex-col p-4">
+      <ul className="space-y-4">
+        <li><Link to="/" className="text-lg hover:text-gray-300">Dashboard</Link></li>
+        <li><Link to="/overview" className="text-lg hover:text-gray-300">Income</Link></li>
+        <li><Link to="/overview" className="text-lg hover:text-gray-300">Expense</Link></li>
+        <li><Link to="/overview" className="text-lg hover:text-gray-300">Budgets</Link></li>
+        <li><Link to="/overview" className="text-lg hover:text-gray-300">Reports</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
